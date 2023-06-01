85130137cdba9d1bc3696561068ceff05ced5e55
🐁 <ec2-user@ip-172-31-41-83.us-east-2.compute.internal>
2023-06-01 14:00:10 +0000

    changed pgsql setup from vcf to txt

--- scripts/pgsql_setup.py
+++ scripts/pgsql_setup.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-import pysam
+import csv
 from datetime import datetime
 
 with open('./db_config.txt', 'r') as f:
@@ -16,6 +16,19 @@ conn = psycopg2.connect(
 )
 
 cur = conn.cursor()
+
+def print_database_sizes():
+    cur.execute("SELECT COUNT(*) FROM users")
+    print(f"Number of records in users table: {cur.fetchone()[0]}")
+
+    cur.execute("SELECT COUNT(*) FROM files")
+    print(f"Number of records in files table: {cur.fetchone()[0]}")
+
+    cur.execute("SELECT COUNT(*) FROM snps")
+    print(f"Number of records in snps table: {cur.fetchone()[0]}")
+
+    cur.execute("SELECT COUNT(*) FROM usersnps")
+    print(f"Number of records in usersnps table: {cur.fetchone()[0]}")
 
 def get_or_create_user_id(user_id, sequencing_service):
     """
@@ -70,50 +83,48 @@ def get_or_create_file_id(user_id, filename):
 
     return result[0]
 
-def parse_vcf(vcf_file, user_id, file_id):
+def parse_txt(txt_file, user_id, file_id):
     """
-    Parse a VCF file and insert the data into the database.
+    Parse a txt file and insert the data into the database.
 
-    :param vcf_file: The path to the VCF file.
+    :param txt_file: The path to the txt file.
     :param user_id: The user ID.
     :param file_id: The file ID.
     """
-    vcf_reader = pysam.VariantFile(vcf_file)
+    with open(txt_file, 'r') as f:
+        vcf_reader = csv.reader(f, delimiter='\t')
 
-    for i, rec in enumerate(vcf_reader.fetch()):
-        try:
-            # Insert the SNP into the SNPs table
-            cur.execute(
-                """
-                INSERT INTO snps (rs_id, chromosome_number, position, reference, alternate, qual, filter, info, format)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-                ON CONFLICT DO NOTHING
-                """,
-                (rec.id, rec.chrom, rec.pos, rec.ref, ','.join(rec.alts), rec.qual, ','.join(rec.filter.keys()), str(rec.info), rec.format)
-            )
+        for i, rec in enumerate(vcf_reader):
+            if rec[0].startswith('#'):
+                continue
 
-            # Insert the UserSNP into the UserSNPs table
-            for sample in rec.samples:
-                cur.execute(
-                    """
-                    INSERT INTO usersnps (user_id, file_id, rs_id, genotype)
-                    VALUES (%s, %s, %s, %s)
-                    """,
-                    (user_id, file_id, rec.id, rec.samples[sample]['GT'])
-                )
-        except Exception as e:
-            with open('error_log.txt', 'a') as f:
-                f.write(f"Error inserting SNP {rec.id} from file {vcf_file}: {e}\n")
+            try:
+                with conn:
+                    with conn.cursor() as curs:
+                        # Insert the SNP into the SNPs table
+                        curs.execute(
+                            """
+                            INSERT INTO snps (rs_id, chromosome_number, position, reference, alternate, qual, filter, info, format)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                            ON CONFLICT DO NOTHING
+                            """,
+                            (rec[2], rec[0], rec[1], rec[3], rec[4], rec[5], rec[6], rec[7], rec[8])
+                        )
 
-        if i % 1000 == 0:
-            conn.commit()
+                        # Insert the UserSNP into the UserSNPs table
+                        curs.execute(
+                            """
+                            INSERT INTO usersnps (user_id, file_id, rs_id, genotype)
+                            VALUES (%s, %s, %s, %s)
+                            """,
+                            (user_id, file_id, rec[2], rec[9])
+                        )
 
-    conn.commit()
+            except Exception as e:
+                print(f"Error inserting SNP {rec[2]} from file {txt_file}: {e}\n")
 
-vcf_dir = '../../genomeprep/opensnp'
-
-for filename in os.listdir(vcf_dir):
-    if filename.endswith('.vcf'):
+def process_file(filename, vcf_dir):
+    if filename.endswith('.txt'):
         _, sequencing_service, user_id, __ = filename.split('.')
         user_id = int(user_id)
 
@@ -121,8 +132,22 @@ for filename in os.listdir(vcf_dir):
 
         file_id = get_or_create_file_id(user_id, filename)
 
-        parse_vcf(os.path.join(vcf_dir, filename), user_id, file_id)
+        parse_txt(os.path.join(vcf_dir, filename), user_id, file_id)
 
+        return True
+    return False
+
+vcf_dir = '../../genomeprep/opensnp_txt'
+files = os.listdir(vcf_dir)
+file_count = len([f for f in files if f.endswith('.txt')])
+processed_files = 0
+
+for filename in files:
+    if process_file(filename, vcf_dir):
+        processed_files += 1
+        if processed_files % 1 == 0:
+            print(f"Processed {processed_files} out of {file_count} files.")
+            print_database_sizes()
 
 cur.close()
 conn.close()
